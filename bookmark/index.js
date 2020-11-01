a8bb2b8f8d583e7945a63feaeeb8dc01fd2975d9
Ching367436 <wang212232003@gmail.com>
2020-11-01 17:20:21 +0800

    sync

--- bookmark/index.js
+++ bookmark/index.js
@@ -1,12 +1,10 @@
-(async () => {
-    async function appendScript(src, defer = false, async = false) {
+(() => {
+    function appendScript(src) {
         const s = document.createElement("script");
         s.src = src;
-        s.defer = defer;
-        s.async = async;
         document.head.appendChild(s);
     }
 
-    appendScript("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.7.0/dist/tf.min.js", async = true);
-    appendScript("https://Ching367436.github.io/validate-tf/bookmark/model.js", defer = true);
+    appendScript("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.7.0/dist/tf.min.js");
+    appendScript("https://Ching367436.github.io/validate-tf/bookmark/model.js");
 })();
