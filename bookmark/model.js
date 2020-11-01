2e2f9af3a5ad074e3dff594ad9635e3ee2a226e9
Ching367436 <wang212232003@gmail.com>
2020-11-01 08:19:20 +0800

    validatPic.addEventListener load

--- bookmark/model.js
+++ bookmark/model.js
@@ -15,22 +15,30 @@
         return String.fromCharCode(n + 55);
     }
 
-    const c = document.createElement("canvas")
-    c.height = height;
-    c.width = width;
-    c.style.display = "none";
-    document.body.appendChild(c);
+    function main() {
 
-    const ctx = c.getContext("2d")
+        const c = document.createElement("canvas")
+        c.height = height;
+        c.width = width;
+        c.style.display = "none";
+        document.body.appendChild(c);
 
-    // crop image
-    let res = "";
-    for (let i = 0; i < 4; i++) {
-        ctx.drawImage(validatePic, -5 - step * i, -1)
-        data = tf.argMax(model.predict(tf.browser.fromPixels(c).expandDims(0)), 1).dataSync();
-        data = int_to_ch(data)
-        res += data;
+        const ctx = c.getContext("2d")
+
+        // crop image
+        let res = "";
+        for (let i = 0; i < 4; i++) {
+            ctx.drawImage(validatePic, -5 - step * i, -1)
+            data = tf.argMax(model.predict(tf.browser.fromPixels(c).expandDims(0)), 1).dataSync();
+            data = int_to_ch(data)
+            res += data;
+        }
+        validateCode.value = res;
+
+        document.body.removeChild(c);
     }
-    validateCode.value = res;
-    
+
+    main();
+
+    validatePic.addEventListener("load", main);
 })();
