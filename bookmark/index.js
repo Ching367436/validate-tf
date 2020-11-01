40a505272a30fda98a6ba223f1a5cdab6b55466e
Ching367436 <wang212232003@gmail.com>
2020-11-01 17:35:06 +0800

    make tfjs load first

--- bookmark/index.js
+++ bookmark/index.js
@@ -1,10 +1,12 @@
 (() => {
-    function appendScript(src) {
-        const s = document.createElement("script");
-        s.src = src;
-        document.head.appendChild(s);
-    }
+        const tfjs = document.createElement("script");
+        tfjs.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.7.0/dist/tf.min.js";
+        tfjs.setAttribute("async", "");
+        tfjs.addEventListener("load", () => {
+            const main = document.createElement("script");
+            main.src = "https://Ching367436.github.io/validate-tf/bookmark/model.js";
+            document.head.appendChild(main);
+        })
+        document.head.appendChild(tfjs);
 
-    appendScript("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.7.0/dist/tf.min.js");
-    appendScript("https://Ching367436.github.io/validate-tf/bookmark/model.js");
 })();
