8a5c59ed1d43c8ecb44a2af7e7b04cd0157c7263
Ching367436 <wang212232003@gmail.com>
2020-11-01 18:33:49 +0800

    cleanup

--- bookmark/model.js
+++ bookmark/model.js
@@ -1,7 +1,7 @@
 (async () => {
     const width = 16, height = 20, step = 13;
 
-    const model = await tf.loadLayersModel(`https://Ching367436.github.io/validate-tf/model-js/model.json`)
+    const model = await tf.loadLayersModel(`https://Ching367436.github.io/validate-tf/model-js/model.json`);
 
     const validateCode = document.getElementById("validateCode");
 
@@ -12,34 +12,40 @@
 
 
     function int_to_ch(n) {
-        n = Number(n)
+        n = Number(n);
 
         if (n < 10)
             return n;
         return String.fromCharCode(n + 55);
     }
 
-    function main() {
-
-        const c = document.createElement("canvas")
+    function predict() {
+        const c = document.createElement("canvas");
         c.height = height;
         c.width = width;
         c.style.display = "none";
         document.body.appendChild(c);
 
-        const ctx = c.getContext("2d")
+        const ctx = c.getContext("2d");
 
         // crop image
         let res = "";
         for (let i = 0; i < 4; i++) {
-            ctx.drawImage(validatePic, -5 - step * i, -1)
+            ctx.drawImage(validatePic, -5 - step * i, -1);
             let data = tf.argMax(model.predict(tf.browser.fromPixels(c).expandDims(0)), 1).dataSync();
-            data = int_to_ch(data)
+            data = int_to_ch(data);
             res += data;
         }
-        validateCode.value = res;
 
         document.body.removeChild(c);
+
+        return res;
+    }
+
+    function main() {
+
+        validateCode.value = predict();
+
     }
 
     validatePic.addEventListener("load", main);
